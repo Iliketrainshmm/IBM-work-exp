98ce1c29b6f20c13ddf645dde34268ab0b0bd88b
Iliketrainshmm <marnell.joshua@gmail.com>
2024-07-23 10:16:27 +0100

    apictesttools

--- uh.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import time
-import random
-currenttimeepoch = int(time.time())
-oldtime = currenttimeepoch-2332800
-randomtime = random.randint(oldtime,currenttimeepoch)
-randomtimedate = datetime.datetime.fromtimestamp(randomtime).isoformat()
-print(randomtimedate)
